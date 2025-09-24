ea76dea72d47ee68f7ca8717898a8c34969dfd30
d11416143-source <d11416143@gs.takming.edu.tw>
2025-09-24 10:57:43 +0800

    123
    123

---
+++ 16號/16號/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace _16號
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.Read();
+        }
+    }
+}
